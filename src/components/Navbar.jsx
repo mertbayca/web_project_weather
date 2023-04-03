4b73d7fd3b1533770ca459d339c16db4a85c666a
mertbayca <103841104+mertbayca@users.noreply.github.com>
2023-04-03 16:16:33 +0300

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="logo">
+            <img src=
+        </div>
+        <div className="w_welcome">
+            <h2>Welcome.</h2>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
